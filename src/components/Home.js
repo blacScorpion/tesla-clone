78e85a2b3d91d323d4c61f7a26655a107707b2d9
blacScorpion <41795126+blacScorpion@users.noreply.github.com>
2022-01-28 10:26:52 +0000

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import Section from './Section';
+
+function Home() {
+  return <Container>
+      <Section
+      title="Model S"
+      description="Order Online for Touchless Delivery"
+      backgroundImg="/images/model-s.jpg"
+      leftBtnText="Custom Order"
+      rightBtnText="Existing Inventory"
+      />
+      <Section
+      title="Model Y"
+      description="Order Online for Touchless Delivery"
+      backgroundImg="/images/model-y.jpg"
+      leftBtnText="Custom Order"
+      rightBtnText="Existing Inventory"
+      />
+      <Section
+      title="Model 3"
+      description="Order Online for Touchless Delivery"
+      backgroundImg="/images/model-3.jpg"
+      leftBtnText="Custom Order"
+      rightBtnText="Existing Inventory"
+      />
+      <Section
+      title="Model X "
+      description="Order Online for Touchless Delivery"
+      backgroundImg="/images/model-x.jpg"
+      leftBtnText="Custom Order"
+      rightBtnText="Existing Inventory"
+      />
+
+<Section
+      title="Lowest Cost Solar Panels in America "
+      description="Money-back gurantee"
+      backgroundImg="/images/solar-panel.jpg"
+      leftBtnText="Order now"
+      rightBtnText="Learn more"
+      />
+      
+      <Section
+      title="Solar for New Roofs "
+      description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+      backgroundImg="/images/solar-roof.jpg"
+      leftBtnText="Order now"
+      rightBtnText="Learn more"
+      />
+
+<Section
+      title="Accessories"
+      
+      backgroundImg="/images/accessories.jpg"
+      leftBtnText="Shop now"
+    
+      />
+  </Container>;
+}
+
+export default Home;
+
+const Container = styled.div`
+height: 100vh;
+
+`
